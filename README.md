7ddaec65578e50c7c971df9ee02f00eb3263f864
Scott <saltyhacker@gmail.com>
2015-03-10 16:45:25 -0600

    Uploaded 64 and 32 bit scripts

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # LinuxProcessStrings
-A forensic tool to parse strings out of a process's mapped memory
+A forensic tool to parse strings out of a process's mapped memory on Linux
+
+# Usage
+Select the correct script: linuxProcessStrings.py (64-bit) or linuxProcessStrings32bit.py (32-bit).
+Give the script execute rights (chmod 755 linuxProcessStrings.py)
+sudo ./linuxProcessStrings.py [PID]
+(The script must be run as root.)
+
+# About
+This will output any Extended ASCII strings (greater than 4 characters) in the process's readable memory regions to the console.
+The output can be piped to a file or through netcat.
+
+Tested on Ubuntu 14.04 (64 bit) and Ubuntu 12.04 (32-bit)
+
+Use at your own risk. Some processes do not take well to being paused.
---
+++ linuxProcessStrings.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#Memory region parser based off of "Gilles" script here - http://unix.stackexchange.com/questions/6301/how-do-i-read-from-proc-pid-mem-under-linux
+
+import re
+import os
+import signal
+import sys
+
+def parsemem(data):
+	#find all strings > length 4 of high-bit characters
+	return re.findall("[^\x00-\x1F\x7F-\xFF]{4,}", data)
+
+def explore(processid):
+	#Pause the process to examine
+	os.kill(int(processid), signal.SIGSTOP)
+
+	#Open the file of process memory maps
+	maps_file = open("/proc/"+processid+"/maps", 'r')
+	mem_file = open("/proc/"+processid+"/mem", 'r', 0)
+
+	#Parse each region of a process memory and print it out
+	for line in maps_file.readlines():  # for each mapped memory region in the process
+		m = re.match(r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) ([-r])', line)
+		#only attempt if region is read-enabled
+		if m.group(3) == 'r':
+			#Start memory address
+			start = long(m.group(1), 16)
+
+			#Stop memory address
+			end = long(m.group(2), 16)
+			#Temporary fix for int overflow in the vsyscall region
+			if start > sys.maxint:
+				continue
+			mem_file.seek(start)  #seek to region start
+			chunk = mem_file.read(end - start)  # read region contents
+			
+			#print the list of matching strings to the regex
+			for found_str in parsemem(chunk):
+				print found_str
+	#close maps and mem
+	maps_file.close()
+	mem_file.close()
+	
+	#Free the process
+	os.kill(int(processid), signal.SIGCONT)
+
+def main(argv):
+	if len(argv) < 2:
+		print "Syntax: ./linuxProcessStrings.py [PID]"
+		return
+	else:
+		explore(argv[1])
+
+if __name__ == "__main__":
+    main(sys.argv)
+
